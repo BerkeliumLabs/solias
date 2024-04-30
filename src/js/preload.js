107c8720569a4484385289c4cc5f68c1b557857a
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-30 20:32:48 +0530

    initializing grapesjs

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,8 +1,9 @@
 const { ipcRenderer } = require("electron");
 const fs = require('fs');
+const grapesjs = require('grapesjs');
 
 window.onload = () => {
-    const controlElements = {
+    /* const controlElements = {
         openFileBtn: document.querySelector('#aside-action-open'),
         saveFileBtn: document.querySelector('#aside-action-save'),
         textArea: document.querySelector('textarea')
@@ -27,5 +28,20 @@ window.onload = () => {
             }
         }
         console.log(fileInfo);
+    }); */
+
+    const editor = grapesjs.init({
+        // Indicate where to init the editor. You can also pass an HTMLElement
+        container: '#gjs',
+        // Get the content for the canvas directly from the element
+        // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+        fromElement: true,
+        // Size of the editor
+        height: '300px',
+        width: 'auto',
+        // Disable the storage manager for the moment
+        storageManager: false,
+        // Avoid any default panel
+        panels: { defaults: [] },
     });
 }
