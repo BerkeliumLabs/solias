4ce29b00278a11a0b9abb3ff2bbe55537f10311d
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-01 19:15:24 +0530

    initialize panels

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,9 @@
 import grapesjs from 'grapesjs';
 import { SOLIAS_BLOCKS } from './_shared/blocks';
+import { SoliasPanels } from './classes/panels';
 
 window.onload = () => {
+
     const editor = grapesjs.init({
         // Indicate where to init the editor. You can also pass an HTMLElement
         container: '#gjs',
@@ -21,5 +23,8 @@ window.onload = () => {
             blocks: SOLIAS_BLOCKS
         },
     });
+
+    const soliasPanels = new SoliasPanels(editor);
+    soliasPanels.loadPanels();
 
 };
