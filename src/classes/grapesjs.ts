56a3c6cc30414663f9bee63596a387122c8f893c
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-04 00:10:48 +0530

    add style selector constant

--- src/classes/grapesjs.ts
+++ src/classes/grapesjs.ts
@@ -1,5 +1,6 @@
 import grapesjs, { Editor } from "grapesjs";
 import { SOLIAS_BLOCKS } from "../shared/constants/blocks";
+import { SOLIAS_STYLE_SELECTORS } from "../shared/constants/style-selectors";
 
 export class SoliasGrapesJS {
     public editor: Editor;
@@ -81,47 +82,7 @@ export class SoliasGrapesJS {
             },
             styleManager: {
                 appendTo: '#style-palette',
-                sectors: [
-                    {
-                        name: 'Dimension',
-                        open: false,
-                        // Use built-in properties
-                        buildProps: ['width', 'min-height', 'padding'],
-                        // Use `properties` to define/override single property
-                        properties: [
-                            {
-                                // Type of the input,
-                                // options: integer | radio | select | color | slider | file | composite | stack
-                                type: 'integer',
-                                name: 'The width', // Label for the property
-                                property: 'width', // CSS property (if buildProps contains it will be extended)
-                                units: ['px', '%'], // Units, available only for 'integer' types
-                                defaults: 'auto', // Default value
-                                min: 0, // Min value, available only for 'integer' types
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Extra',
-                        open: false,
-                        buildProps: ['background-color', 'box-shadow', 'custom-prop'],
-                        properties: [
-                            {
-                                id: 'custom-prop',
-                                name: 'Custom Label',
-                                property: 'font-size',
-                                type: 'select',
-                                defaults: '32px',
-                                // List of options, available only for 'select' and 'radio'  types
-                                options: [
-                                    { id: "1", value: '12px', name: 'Tiny' },
-                                    { id: "1", value: '18px', name: 'Medium' },
-                                    { id: "1", value: '32px', name: 'Big' },
-                                ],
-                            }
-                        ]
-                    }
-                ]
+                sectors: SOLIAS_STYLE_SELECTORS
             }
         });
 
