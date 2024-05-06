a8a6356bd3e1bbbe2edd5335a4b8e3e86041c6c2
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-06 23:36:42 +0530

    adding form element blocks

--- src/shared/constants/blocks.ts
+++ src/shared/constants/blocks.ts
@@ -1,4 +1,36 @@
 export const SOLIAS_BLOCKS = [
+    // Basic elements
+    {
+        id: 'div',
+        label: 'Div',
+        content: '<div></div>',
+        category: 'Basic'
+    },
+    {
+        id: 'span',
+        label: 'Span',
+        content: '<span></span>',
+        category: 'Basic'
+    },
+    {
+        id: 'p',
+        label: 'Paragraph',
+        content: '<p></p>',
+        category: 'Basic'
+    },
+    {
+        id: 'h1',
+        label: 'Headings',
+        content: '<h1>Title</h1>',
+        category: 'Basic'
+    },
+    // Form Elements
+    {
+        id: 'form',
+        label: 'Form',
+        content: '<form></form>',
+        category: 'Form Elements'
+    },
     {
         id: 'button', // id is mandatory
         label: 'Button', // You can use HTML/SVG inside labels
@@ -13,11 +45,72 @@ export const SOLIAS_BLOCKS = [
         category: 'Form Elements'
     },
     {
+        id: 'input-checkbox',
+        label: 'Checkbox',
+        content: `<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="ch-1">
+        <label class="form-check-label" for="ch-1">
+          Checkbox
+        </label>
+      </div>`,
+        category: 'Form Elements'
+    },
+    {
+        id: 'input-radio',
+        label: 'Radio',
+        content: `<div class="form-check">
+        <input class="form-check-input" type="radio" id="radio-1">
+        <label class="form-check-label" for="radio-1">
+          Radio
+        </label>
+      </div>`,
+        category: 'Form Elements'
+    },
+    {
+        id: 'label',
+        label: 'Label',
+        content: '<label>Label</label>',
+        category: 'Form Elements'
+    },
+    {
+        id: 'select',
+        label: 'Select',
+        content: '<select class="form-control"></select>',
+        category: 'Form Elements'
+    },
+    {
+        id: 'datalist',
+        label: 'Data List',
+        content: '<datalist></datalist>',
+        category: 'Form Elements'
+    },
+    {
+        id: 'option',
+        label: 'Options',
+        content: {
+            type: 'text',
+            tagName: 'option',
+            traits: ['value', 'label'],
+            attributes: {
+                value: 'solias',
+                label: 'Solias'
+            }
+        },
+        category: 'Form Elements',
+    },
+    {
         id: 'textrea',
         label: 'Textarea',
         content: '<textarea class="form-control"></textarea>',
         category: 'Form Elements'
     },
+    {
+        id: 'fieldset',
+        label: 'Fieldset',
+        content: '<fieldset><legend>Legend</legend></fieldset>',
+        category: 'Form Elements'
+    },
+    // Media elements
     {
         id: 'image',
         label: 'Image',
