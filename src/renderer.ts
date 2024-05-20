42d2a12e04a3cb5ec84d0f4160d467d78d9f9e76
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-20 22:29:03 +0530

    add navigation bar

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,22 @@
 /* Solias Styles */
 import './scss/index.scss';
 
-// console.log('👋 Welcome to Solias');
+/* View Functions */
+
+const navItems = document.querySelectorAll('.solias-core__nav-item');
+const viewPanels = document.querySelectorAll('.view-panel');
+
+navItems.forEach((item: HTMLLIElement) => {
+    item.addEventListener('click', (ev: MouseEvent) => changeView(ev, item));
+});
+
+function changeView(evt: Event, listItem: HTMLLIElement) {
+    navItems.forEach((item: HTMLLIElement) => item.classList.remove('nav-item--active'));
+    listItem.classList.add('nav-item--active');
+    const view = Array.from(viewPanels).find((item: HTMLDivElement) => item.id == listItem.getAttribute('data-view'));
+    viewPanels.forEach((item: HTMLDivElement) => item.style.display = 'none');
+    if (view) {
+      (view as HTMLDivElement).style.display = 'flex';
+    }
+    console.log(evt, listItem);
+}
