a4772efe9b17c8610aeb5923c8fa0e2d4dc75bdd
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-20 22:07:20 +0530

    update fonts and styles

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,4 @@
-/* GrapesJS Default Styles */
-import 'grapesjs/dist/css/grapes.min.css';
 /* Solias Styles */
-import '@berkelium/primordial-theme/dist/styles.min.css';
 import './scss/index.scss';
 
 // console.log('👋 Welcome to Solias');
